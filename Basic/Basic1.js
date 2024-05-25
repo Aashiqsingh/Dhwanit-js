94c70ae10c7568756ea994811f85aa337a39f71c
Aashiqsingh <aashiqsingh56789@gmail.com>
2024-05-25 12:02:44 +0530

    basic

---
+++ Basic/Basic1.js
@@ -0,0 +1,33 @@
+// print 
+
+// console.log("Hello world!!!");
+// console.info("Hello information!!");
+// console.warn("Hello warning...");
+// console.error("Erro 404 not found!!!")
+
+// js have no data tyep 
+
+
+// Data type --> Number,string , boolean , undefined , null 
+
+a = 10
+
+console.log("value of a = ",a);
+console.log("Type of a = ",typeof a);
+
+b = 20.56
+
+console.log("Value of b = ",b);
+console.log("Type of b = ",typeof(b));
+
+c = "ram",
+console.log("Value of c = ",c);
+console.log("Type of c = ",typeof(c));
+
+isActive = true;
+console.log("Value of isActive = ",isActive);
+console.log("Type of isActive = ",typeof(isActive));
+
+let x 
+
+console.log("value of x = ",x);
