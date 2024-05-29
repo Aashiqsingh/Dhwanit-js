b58061c6bc411b6547b0eb7924c9541d3fa86aee
Aashiqsingh <aashiqsingh56789@gmail.com>
2024-05-29 12:55:13 +0530

    input , operator

---
+++ input/input.js
@@ -0,0 +1,27 @@
+// var a = 10
+// var b = 12
+
+
+// console.log("Addition = "+(a+b));
+
+
+// Output :
+
+// alert("Hello world!!!");
+
+// confirmation :
+// var x = confirm("Are you single.")
+// console.log(x);
+
+// input :
+
+// var x = parseInt(prompt("Enter the number :"));
+// var y = parseInt(prompt("Enter the number :"));
+
+var x = prompt("Enter the number :");
+var y = prompt("Enter the number :");
+
+
+
+// console.log("Addition = ",parseInt(x)+parseInt(y));
+
