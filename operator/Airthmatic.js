cc4d8615b0ebf8076853708f3a578edb90d1d5ea
Aashiqsingh <aashiqsingh56789@gmail.com>
2024-06-01 11:32:50 +0530

    airthmatic,relational

---
+++ operator/Airthmatic.js
@@ -0,0 +1,9 @@
+var a = 3
+var b = 4
+
+console.log("Addition = ",a+b);
+console.log("subtraction = ",a-b);
+console.log("multiplication = ",a*b);
+console.log("division = ",a/b);
+console.log("module = ",a%b);
+console.log("Exponent = ",a**b);
---
+++ operator/Relational.js
@@ -0,0 +1,14 @@
+var a = 10
+var b = 10
+
+
+
+// console.log("gretter than = ",a>b);
+// console.log("less than = ",a<b);
+console.log("double equal to = ",a==b); // true
+// console.log("gretter than equal to = ",a<=b);
+// console.log("not equal to = ",a!=b);
+console.log("triple equal to = ",a === b); // true
+
+console.log("not equal to = ",a!=b);
+console.log("double not eual to = ",a !== b);
