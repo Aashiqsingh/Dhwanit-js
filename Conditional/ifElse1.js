e464383146071038f286b44a9a5aa67e8be410ea
Aashiqsingh <aashiqsingh56789@gmail.com>
2024-06-01 12:48:34 +0530

    if else

--- Conditional/ifElse1.js
+++ Conditional/ifElse1.js
@@ -15,24 +15,52 @@ var age = 61;
 var isChild = true
 
 
-if(isAlive === true){
-    console.log("you are alive");
-    if(age > 60){
-        console.log("you are old");
-        if(isChild === true){
-            console.log("you have a child")
-        }
-        else{
-            console.log("you have not a child")
-        }
-    }
-    else if(age > 18){
-        console.log("you are adult");
-    }
-    else{
-        console.log("you are kids.");
-    }
-}
-else{
-    console.log("you are dead");
-    }
+// if(isAlive === true){
+//     console.log("you are alive");
+//     if(age > 60){
+//         console.log("you are old");
+//         if(isChild === true){
+//             console.log("you have a child")
+//         }
+//         else{
+//             console.log("you have not a child")
+//         }
+//     }
+//     else if(age > 18){
+//         console.log("you are adult");
+//     }
+//     else{
+//         console.log("you are kids.");
+//     }
+// }
+// else{
+//     console.log("you are dead");
+// }
+
+// switch(isAlive){
+//     case true: 
+//         console.log("you are alive");
+//         break;
+
+//     case false:
+//         console.log("you are dead");
+//         break;
+
+//     default:
+//         console.log("you are not alive or dead");
+// }
+
+// let num = 25
+
+// switch(num%2==0){
+//     case true:
+//         console.log("even number");
+//         break;
+
+//     case false:
+//         console.log("odd number");
+//         break;
+// }
+
+
+prompt("1 for addition \n 2 for subtraction \n 3 for multiplication \n 4 for division \n\n\n Enter your choice:-")
