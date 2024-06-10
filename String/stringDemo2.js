2e4532ca2b039ad47d3381e06269ba539aca45f4
Aashiqsingh <aashiqsingh56789@gmail.com>
2024-06-10 12:37:10 +0530

    stringMethod

---
+++ String/stringDemo2.js
@@ -0,0 +1,35 @@
+var str = "    Royal technosoft pvt ltd.      "
+console.log(str);
+console.log(str.length);
+
+// var x = str.repeat(2)
+// console.log(x);
+
+
+// var x = str.replace("y" ,"u")
+// var x = str.replace(".","Priva123")
+
+// console.log(x);
+
+// var x = str.slice(1)
+// var y = str.slice(2,5)
+// console.log(y);
+// var x = str.slice(-5 , -2)
+
+
+
+// var x = str.split("o")
+// console.log(x);
+
+// var x = str.substring(2 , 5)
+// console.log(x);
+
+// var x = str.toLocaleLowerCase()
+// var x = str.toLocaleUpperCase()
+// var x = str.toString()
+
+
+// var x = str.trim()
+var x = str.trimStart()
+console.log(x);
+console.log(x.length);
