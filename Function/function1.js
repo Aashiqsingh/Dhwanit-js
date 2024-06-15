ae02b27ebd0fa462b5b928128b511117a68be206
Aashiqsingh <aashiqsingh56789@gmail.com>
2024-06-15 12:45:00 +0530

    function

---
+++ Function/function1.js
@@ -0,0 +1,37 @@
+
+// with out return without arguments
+function Demo(){
+    console.log("Demo function called....");
+    
+}
+
+Demo();
+
+// without return with arguments
+
+function name(fname , lname){
+    console.log("name function called...." , fname , lname);
+}
+
+name("dhwanit","garg");
+
+// with return without arguments
+
+function string(){
+
+    return "String function called..."
+}
+
+// var x = string()
+// console.log(x);
+// console.log(string());
+
+// with return with arguments
+
+function add(a,b,c){
+
+    return a+b+c
+}
+
+var x = add(3,4,5)
+console.log("add function called...",x);
--- JSON/json2.js
+++ JSON/json2.js
@@ -13,10 +13,18 @@ var Phone = {
     }
 }
 
-console.log(Phone);
-console.log(Phone.name);
-console.log(Phone.price);
-console.log(Phone.stock);
-console.log(Phone.color);
-console.log(Phone.color[1]);
-console.log(Phone.address.city);
+// console.log(Phone);
+// console.log(Phone.name);
+// console.log(Phone.price);
+// console.log(Phone.stock);
+// console.log(Phone.color);
+// console.log(Phone.color[1]);
+// console.log(Phone.address.city);
+
+// console.log(Phone.color[0]);
+
+
+
+for(let i=0;i<Phone.color.length;i++){
+    console.log(Phone.color[i]);
+}
