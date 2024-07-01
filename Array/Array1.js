e9c0f1dd62c44feee0d37fc33005b592abb18099
Aashiqsingh <aashiqsingh56789@gmail.com>
2024-07-01 12:35:45 +0530

    array

---
+++ Array/Array1.js
@@ -0,0 +1,42 @@
+// // Array  : - hetrogenius 
+
+// var arr = ["Ram" , 45 , "shyam" , true]
+// console.log(arr);
+// // console.log(arr[0]);
+// // console.log(arr[1]);
+
+
+// for(let i=0;i<arr.length;i++){
+//     console.log(arr[i]);
+// }
+
+
+var arr = ["ram","shyam","dhwanit","rahul","hetvi","bhavin","sagar","samir","tejas"]
+console.log(arr);
+
+
+var user = ["kunal","manipal","samarth","balwinder"]
+
+for(let i=0;i<user.length;i++){
+    console.log(arr.push(user[i]));
+}
+
+console.log(arr);
+
+
+// var x = arr.push("aashiq","harsh")
+// console.log(x);
+// console.log(arr);
+
+// var x = arr.pop();
+
+// console.log(x);
+
+
+// arr.unshift("shruti")
+
+// console.log(arr);
+
+
+// arr.shift();
+// console.log(arr);
---
+++ Array/Array2.js
@@ -0,0 +1,24 @@
+var arr = ["ram","shyam","dhwanit","rahul", 12 ,"hetvi","bhavin","sagar","samir","tejas"]
+
+// console.log(typeof arr);
+
+var user = ["kunal","manipal","samarth","balwinder"]
+
+// var x = arr.concat(user)
+// console.log(x);
+
+
+// var x = arr.reverse()
+// console.log(x);
+
+// var x = arr.toString()
+// console.log(x);
+
+// var x = arr.indexOf(12)
+// console.log(x);
+
+// var x = arr.join('+')
+// console.log(x);
+
+var x = arr.includes("Sagar")
+console.log(x);
