57ae04a2c77caaffed241a1a2251c6153723e08a
Aashiqsingh <aashiqsingh56789@gmail.com>
2024-07-22 12:23:31 +0530

    onclick

---
+++ Dom/Dom1.js
@@ -0,0 +1,11 @@
+// var x = document.getElementById("output")
+// console.log(x.innerHTML);
+
+
+function changeText(){
+
+    var x = document.getElementById("output").innerHTML
+    console.log(x);
+    // x.innerHTML = "Byy Dhwanit...";
+    // x.style.color = "blue"
+}
