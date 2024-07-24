b1253546a236bc0ac7da58c5200f1f51a0a56899
Aashiqsingh <aashiqsingh56789@gmail.com>
2024-07-24 12:41:20 +0530

    onclick , ondblclick

--- Dom/Dom1.js
+++ Dom/Dom1.js
@@ -1,11 +1,25 @@
-// var x = document.getElementById("output")
-// console.log(x.innerHTML);
 
 
 function changeText(){
 
-    var x = document.getElementById("output").innerHTML
-    console.log(x);
-    // x.innerHTML = "Byy Dhwanit...";
-    // x.style.color = "blue"
+    var x = document.getElementById("txt")
+    console.log(x.innerHTML);
+
+    x.innerHTML = "Hello world"
+    x.style.backgroundColor = "blue"
+    x.style.color = "white"
+}
+
+function changeHref(){
+
+    var myLink = document.getElementById("myLink")
+    myLink.href = "https://www.netflix.com"
+    myLink.innerHTML = "Netflix"
+}
+
+function changeSize(){
+    var myImg = document.getElementById("myImg")
+    myImg.style.height = "400px"
+    myImg.style.width = "400px"
+    myImg.src = "https://s7d1.scene7.com/is/image/wbcollab/India_PM_Narendra_Modi-2?qlt=90&fmt=webp&resMode=sharp2"
 }
