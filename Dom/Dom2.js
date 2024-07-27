506e5e684b83b12a0bc994a64d76fe43f87b3eba
Aashiqsingh <aashiqsingh56789@gmail.com>
2024-07-27 11:36:59 +0530

    mouse event

---
+++ Dom/Dom2.js
@@ -0,0 +1,15 @@
+function submitHandler(){
+
+    var btn = document.getElementById('btn');
+
+    console.log("succeessfully submitted");
+    btn.style.backgroundColor = "red"
+
+}
+
+var x = 1
+function changeClr(){
+
+    // console.log("mouse move");
+    console.log(x++);
+}
