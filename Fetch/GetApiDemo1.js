99b59efec1dc300b3115bd6b0b75e6bed847ab81
Aashiqsingh <aashiqsingh56789@gmail.com>
2024-09-11 12:56:51 +0530

    fetch get table

--- Fetch/GetApiDemo1.js
+++ Fetch/GetApiDemo1.js
@@ -9,11 +9,58 @@ const getData = async()=>{
 
     var response = await res.json();
     // console.log(response.data);
-    console.log(response.message);
+    // console.log(response.data);
+
+    var output = document.getElementById("output");
+
+    for(let i=0;i<response.data.length;i++){
+
+        let tr = document.createElement("tr");
+        let idTd = document.createElement("td");
+        let nameTd = document.createElement("td");
+        let emailTd = document.createElement("td");
+        let ageTd = document.createElement("td");
+        let isActiveTd = document.createElement("td");
+
+        idTd.innerHTML = response.data[i]._id;
+        nameTd.innerHTML = response.data[i].name;
+        emailTd.innerHTML = response.data[i].email;
+        ageTd.innerHTML = response.data[i].age;
+        isActiveTd.innerHTML = response.data[i].isActive
+
+
+        output.appendChild(tr);
+        tr.appendChild(idTd);
+        tr.appendChild(nameTd);
+        tr.appendChild(emailTd);
+        tr.appendChild(ageTd);
+        tr.appendChild(isActiveTd);
+
+
+    }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    // for(let i=0;i<response.data.length;i++){
+    //     console.log(response.data[i]);
+        
+    // }
     
     
     
 }
 
-
-getData()
