b677b3a10c7f687dc9d29a26915ee617b9b13463
Aashiqsingh <aashiqsingh56789@gmail.com>
2024-09-14 11:38:27 +0530

    postApi

---
+++ Fetch/PostApi.js
@@ -0,0 +1,42 @@
+const postData = async()=>{
+
+
+    var user = {
+        name:"aashiq",
+        email:"aashiqt@gmail.com",
+        age:23,
+        isActive:true,
+    }
+
+    const response = await fetch("https://node5.onrender.com/user/user/",{
+        method: "POST",
+        headers: {
+            'Content-Type': 'application/json'
+        },
+        // body: JSON.stringify({
+        //     name:"dhwanit",
+        //     email:"dhwanit@gmail.com",
+        //     age:18,
+        //     isActive:true,
+        // })
+
+
+        body: JSON.stringify(user)
+    })
+
+
+    const res = await response.json();
+    console.log(res);
+
+    if(response.status)
+    {
+        alert("Data Posted Successfully");
+    }
+    else{
+        alert("Failed to Post Data");
+    }
+    
+    
+    // console.log(response);
+    
+}
