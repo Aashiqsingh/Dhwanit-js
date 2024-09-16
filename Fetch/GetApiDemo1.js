3625b304077d1f88552282c1a619c83292eff6fe
Aashiqsingh <aashiqsingh56789@gmail.com>
2024-09-16 12:33:12 +0530

    delete api

--- Fetch/GetApiDemo1.js
+++ Fetch/GetApiDemo1.js
@@ -21,12 +21,42 @@ const getData = async()=>{
         let emailTd = document.createElement("td");
         let ageTd = document.createElement("td");
         let isActiveTd = document.createElement("td");
+        let deleteTd = document.createElement("td");
+        let deletebtn = document.createElement("button");
 
         idTd.innerHTML = response.data[i]._id;
         nameTd.innerHTML = response.data[i].name;
         emailTd.innerHTML = response.data[i].email;
         ageTd.innerHTML = response.data[i].age;
         isActiveTd.innerHTML = response.data[i].isActive
+        deletebtn.innerHTML = "Delete";
+        deletebtn.setAttribute("class","btn btn-danger")
+        deletebtn.addEventListener("click",async()=>{
+            // alert("Data deleted successfully..." + response.data[i].name);
+
+            const res1 =await fetch("https://node5.onrender.com/user/user/"+response.data[i]._id,{
+                method:"DELETE",
+                headers:{
+                    "Content-Type":"application/json"
+                },
+                // body:JSON.stringify({_id:response.data[i]._id})
+            })
+
+            console.log(res1);
+            
+
+            // var response1 = await res1.json();
+            // console.log(response1.data);
+            
+
+            if(res1.status === 204){
+                alert("Data deleted successfully..")
+            }
+            else{
+                alert("Failed to delete data...")
+            }
+        })
+
 
 
         output.appendChild(tr);
@@ -35,6 +65,8 @@ const getData = async()=>{
         tr.appendChild(emailTd);
         tr.appendChild(ageTd);
         tr.appendChild(isActiveTd);
+        tr.appendChild(deleteTd);
+        deleteTd.appendChild(deletebtn);
 
 
     }
